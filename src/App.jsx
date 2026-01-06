7be8dc43ffeaa0525d7f3c59967de2ea88c5deaa
blackViking <zare.6984@gmail.com>
2026-01-06 15:11:09 +0330

    small change in addresses

--- src/App.jsx
+++ src/App.jsx
@@ -17,18 +17,22 @@ import Profile from './pages/Profile/Profile'
 function App() {
   return (
     <HashRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/map" element={<Map />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/forgot-password" element={<ForgotPassword />} />
-      <Route path="/reset-password" element={<ResetPassword />} />
-      <Route path="/terms" element={<Terms />} />
-      <Route path="/verify-email" element={<VerifyEmail />} />
-      <Route path="/profile" element={<Profile />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/map" element={<Map />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/terms" element={<Terms />} />
+        <Route path="/verify-email" element={<VerifyEmail />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/add" element={<AddItemPage />} />
+        <Route path="/items" element={<RecentLostItemsPage />} />
+        <Route path="/items/:id/edit" element={<EditItemPage />} />
+        <Route path="/chat/:username" element={<ChatPage />} />
+      </Routes>
     </HashRouter>
   )
 }
