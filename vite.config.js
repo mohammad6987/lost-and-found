80f351590234125f9674500c6e8fe9f1fc868708
blackViking <zare.6984@gmail.com>
2026-02-26 03:41:38 +0330

    adding small change to see if map markers appear or not

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '')
   const authApiTarget = env.VITE_API_BASE_URL || 'https://lostfound1.pythonanywhere.com'
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
 
   return {
     plugins: [react()],
-    base: '/lost-and-found',
+    base: '/lost-and-found/',
     server: {
       cors: true,
       proxy: {
